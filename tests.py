1892906b2cd47cda4b002f635242dd5c8fe4078f
markdavidmc0 <markdavidmc0@gmail.com>
2021-02-23 18:34:07 +0000

    Init pytest and Github actions.

---
+++ tests.py
@@ -0,0 +1,15 @@
+import pytest
+
+class TestScraper():
+
+    def test_sanity(self):
+        pass
+
+class TestPredictor():
+
+    def test_sanity(self):
+        pass
+
+class TestSheets():
+    def test_sanity(self):
+        pass
